c61699d90bf26b0d5e80d5bb316696f4249ad8ab
Volodymyr <Volodumurvel@gmail.com>
2024-08-24 18:58:39 +0200

    add lightbox and webp

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
+import GLightbox from 'glightbox';
+import 'glightbox/dist/css/glightbox.css';
 
 const gallerySliderElement = document.querySelector('.js-gallery-swiper');
 const gallerySliderProps = {
@@ -20,6 +22,25 @@ const gallerySliderProps = {
     onlyInViewport: false,
   },
 };
-
 const gallerySlider = new Swiper(gallerySliderElement, gallerySliderProps);
 gallerySlider.pagination.init();
+
+//dynamic import
+/* document.addEventListener('DOMContentLoaded', async () => {
+  const { createGallerySwiper } = await import('./swiper-config');
+
+  const gallerySwiperElement = document.querySelector('.js-gallery-swiper');
+
+  if (gallerySwiperElement) {
+    const gallerySwiper = createGallerySwiper(gallerySwiperElement);
+    gallerySwiper.pagination.init();
+  }
+}); */
+
+// Initialize GLightbox
+const lightbox = GLightbox({
+  selector: '.glightbox', // This will look for all anchors with this class
+  touchNavigation: true,
+  loop: true,
+  autoplayVideos: true,
+});
