95161c304f10320945764b12331984ff31f1864d
Volodymyr <Volodumurvel@gmail.com>
2024-08-26 22:54:18 +0200

    Update hero-parallax.js

--- src/js/hero-parallax.js
+++ src/js/hero-parallax.js
@@ -1,11 +1,15 @@
 const gearWhite = document.querySelector('.hero-gear-white');
+const gearRed = document.querySelector('.hero-gear-red');
 const robot = document.querySelector('.hero-robot');
 const phone = document.querySelector('.hero-phone');
 const atom = document.querySelector('.hero-atom');
+const h2o = document.querySelector('.hero-h2o');
 const a = document.querySelector('.hero-a');
 const gameName = document.querySelector('.game-name');
 const xo = document.querySelector('.hero-xo');
 const arrowRed = document.querySelector('.arrow-red');
+const arrowWhite = document.querySelector('.hero-arrow-white');
+
 const title = document.querySelector('.hero-title');
 
 window.addEventListener('scroll', function () {
@@ -17,12 +21,20 @@ window.addEventListener('scroll', function () {
   gearWhite.style.transform = `translateX(${scrollPosition * 0.4}px) rotate(${
     scrollPosition * 0.3
   }deg)`;
-  /*   gearWhite.style.transform = `rotate(${scrollPosition * 0.1}deg)`; */
+  gearRed.style.transform = `translateX(${-scrollPosition * 0.4}px) rotate(${
+    -scrollPosition * 0.4
+  }deg)`;
   robot.style.transform = `translateX(${-scrollPosition * 0.3}px)`;
   a.style.transform = `translateX(${-scrollPosition * 0.3}px)`;
   xo.style.transform = `translateX(${scrollPosition * 0.2}px)`;
+  arrowWhite.style.transform = `translateX(${-scrollPosition * 0.2}px) rotate(${
+    scrollPosition * 0.1
+  }deg)`;
   arrowRed.style.transform = `translateX(${scrollPosition * 0.2}px) rotate(${
     scrollPosition * 0.1
+  }deg)`;
+  h2o.style.transform = `translateX(${-scrollPosition * 0.2}px) rotate(${
+    -scrollPosition * 0.1
   }deg)`;
   phone.style.transform = `translateY(${scrollPosition * 0.2}px)`;
   atom.style.transform = `translate(${scrollPosition * 0.1}px, ${
