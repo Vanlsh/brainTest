9309523c4030b6497210edc4f7e03b9d41e0c699
Volodymyr <Volodumurvel@gmail.com>
2024-08-26 20:55:15 +0200

    add swing

--- src/js/hero-parallax.js
+++ src/js/hero-parallax.js
@@ -4,17 +4,24 @@ const phone = document.querySelector('.hero-phone');
 const atom = document.querySelector('.hero-atom');
 const a = document.querySelector('.hero-a');
 const gameName = document.querySelector('.game-name');
+const xo = document.querySelector('.hero-xo');
+const arrowRed = document.querySelector('.arrow-red');
+const title = document.querySelector('.hero-title');
 
-a.style.transform = 'scale(1.5)';
 window.addEventListener('scroll', function () {
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 100) {
     gameName.classList.add('text-pop-up-top');
   }
-
-  gearWhite.style.transform = `rotate(${scrollPosition * 0.1}deg)`;
+  gearWhite.style.transform = `translateX(${scrollPosition * 0.4}px) rotate(${
+    scrollPosition * 0.3
+  }deg)`;
+  /*   gearWhite.style.transform = `rotate(${scrollPosition * 0.1}deg)`; */
   robot.style.transform = `translateX(${-scrollPosition * 0.3}px)`;
+  a.style.transform = `translateX(${-scrollPosition * 0.3}px)`;
+  xo.style.transform = `translateX(${scrollPosition * 0.2}px)`;
+  arrowRed.style.transform = `translateX(${scrollPosition * 0.2}px)`;
   phone.style.transform = `translateY(${scrollPosition * 0.2}px)`;
   atom.style.transform = `translate(${scrollPosition * 0.1}px, ${
     scrollPosition * 0.05
