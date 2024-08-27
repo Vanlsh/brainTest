7f6a9db6715ec624b044710cf5868ab4777bb675
Volodymyr <Volodumurvel@gmail.com>
2024-08-27 23:43:40 +0200

    add lazy load swiper

--- src/js/reviews.js
+++
@@ -1,7 +0,0 @@
-import { createReviewsSwiper } from './swiper-config';
-
-const reviewsSwiperElement = document.querySelector('.js-reviews-swiper');
-
-if (reviewsSwiperElement) {
-  createReviewsSwiper(reviewsSwiperElement);
-}
